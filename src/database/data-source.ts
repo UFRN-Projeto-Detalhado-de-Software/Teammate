a68f437c934f9c8b802a5700f3cabbe20a969fbc
jontvrs <jonathan.silva@grupoacert.com.br>
2023-10-25 19:16:12 -0300

    Adiciona migrations e entidade para registro de equipes

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -8,6 +8,9 @@ import { CreateUsersTable1695281639949 } from "./migrations/1695281639949-Create
 import { CreateClassroomsTable1695903687240 } from "./migrations/1695903687240-CreateClassroomsTable"
 import { CreateUsersTokensTable1698006547888 } from "./migrations/1698006547888-CreateUsersTokensTable"
 import { CreateClassroomsUsersTable1698142184197 } from "./migrations/1698142184197-CreateClassroomsUsersTable"
+import { CreateClassroomTeamTable1698162930395 } from "./migrations/1698162930395-CreateClassroomTeamTable"
+import { CreateClassroomTeamsUsersTable1698162939369 } from "./migrations/1698162939369-CreateClassroomTeamsUsersTable"
+import { ClassroomTeam } from "../modules/classrooms/entities/ClassroomTeam"
 
 export const AppDataSource = new DataSource({
   type: "postgres",
@@ -18,12 +21,14 @@ export const AppDataSource = new DataSource({
   database: "teammate",
   synchronize: true,
   logging: false,
-  entities: [User, UserTokens, Classroom],
+  entities: [User, UserTokens, Classroom, ClassroomTeam],
   migrations: [
     CreateUsersTable1695281639949, 
     CreateUsersTokensTable1698006547888, 
     CreateClassroomsTable1695903687240,
-    CreateClassroomsUsersTable1698142184197
+    CreateClassroomsUsersTable1698142184197,
+    CreateClassroomTeamTable1698162930395,
+    CreateClassroomTeamsUsersTable1698162939369,
   ],
   subscribers: [],
 })
